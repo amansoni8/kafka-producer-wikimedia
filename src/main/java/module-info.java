5456ad0f1289fc99737131587392542e5dfc9dc1
aman <amansoni22103@gmail.com>
2024-03-26 23:08:24 +0530

    new commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module org.example.kafkaproducerwikimedia {
     requires javafx.controls;
     requires javafx.fxml;
+    requires kafka.clients;
+    requires java.desktop;
+    requires okhttp.eventsource;
 
 
-    opens org.example.kafkaproducerwikimedia to javafx.fxml;
-    exports org.example.kafkaproducerwikimedia;
+    opens org.example.wikimedia.kafkaproducerwikimedia to javafx.fxml;
+    exports org.example.wikimedia.kafkaproducerwikimedia;
 }
---
+++ src/main/java/org/example/wikimedia/kafkaproducerwikimedia/WikimediaChangeHandler.java
@@ -0,0 +1,13 @@
+package org.example.wikimedia.kafkaproducerwikimedia;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+public class WikimediaChangeHandler implements EventHandler {
+
+    @Override
+    public void onOpen() {
+
+    }
+}
+}
